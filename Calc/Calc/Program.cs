e011e86368b9d84ccf5e177e4ee128d821285cc4
NipL3g3nd <ecaplin27@gmail.com>
2024-05-20 22:35:34 +0300

    First version calc

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в простой калькулятор!");
+
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите операцию (+, -, *, /): ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    if (number2 != 0)
+                    {
+                        result = number1 / number2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: неверная операция!");
+                    return;
+            }
+
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
